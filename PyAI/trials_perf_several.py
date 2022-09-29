5841c8aabfce98d759cc56d756738a92d4b08a57
Côme ANNICCHIARICO <annicchiarico.come@gmail.com>
2022-09-29 10:46:22 +0200

    Terrible feedback effect

--- PyAI/trials_perf_several.py
+++ PyAI/trials_perf_several.py
@@ -164,12 +164,24 @@ if __name__=="__main__":
     behaviour_error_mean = sliding_window_mean(error_behaviour,smooth_window)
     error_obs_mean = sliding_window_mean(error_observations,smooth_window)
 
+    state_error_mean_ok = sliding_window_mean(error_states_ok,smooth_window)
+    behaviour_error_mean_ok = sliding_window_mean(error_behaviour_ok,smooth_window)
+    error_obs_mean_ok = sliding_window_mean(error_observations_ok,smooth_window)
+
+    state_error_mean_imp = sliding_window_mean(error_states_imp,smooth_window)
+    behaviour_error_mean_imp = sliding_window_mean(error_behaviour_imp,smooth_window)
+    error_obs_mean_imp = sliding_window_mean(error_observations_imp,smooth_window)
+
+    state_error_mean_sham = sliding_window_mean(error_states_sham,smooth_window)
+    behaviour_error_mean_sham = sliding_window_mean(error_behaviour_sham,smooth_window)
+    error_obs_mean_sham = sliding_window_mean(error_observations_sham,smooth_window)
+
     color1 = 'red'
     color2 = 'blue'
+    color3 = 'green'
+    color4 = 'orange'
     fig_main = plt.figure()
-        
-    fig = fig_main
-    ax1 = fig.add_subplot(211)
+    ax1 = fig_main.add_subplot(111)
     #ax1.grid(True, which='both')
     
     ax1.spines['right'].set_color('none')
@@ -186,7 +198,7 @@ if __name__=="__main__":
     ax1.tick_params(axis='y', labelcolor="black")
 
     #ax1.set_xlabel('Trials',fontsize=10)
-    ax1.set_ylabel('MODEL ERROR/ENTROPY', color="black",fontsize=10)
+    ax1.set_ylabel('ACTION MODEL ERROR', color="black",fontsize=10)
     ax1.set_ylim([-0.1,1.1])
     
 
@@ -195,29 +207,57 @@ if __name__=="__main__":
     # ax2.set_ylabel('MODEL ERROR (kl divergence)', color="black",fontsize=10)  # we already handled the x-label with ax1
     # ax2.set_ylim([-0.1,1.1])
     wdt = 2.5
-    l1 = ax1.plot(trials, a_unc, color=color1,label='A entropy',ls='--',linewidth=wdt/2.0)
-    l2 = ax1.plot(trials, b_unc, color=color2,label='B entropy',ls='--',linewidth=wdt/2.0)
     # instantiate a second axes that shares the same x-axis
     
-    l3 = ax1.plot(trials, a_err, color=color1,label='A error',ls='-',linewidth=wdt)
-    l4 = ax1.plot(trials, b_err, color=color2,label='B error',ls='-',linewidth=wdt)
+    l1 = ax1.plot(trials, b_err, color=color1,label='PERFECT',ls='-',linewidth=wdt)
+    l2 = ax1.plot(trials, b_err_ok, color=color2,label='NOISY',ls='-',linewidth=wdt)
+    l3 = ax1.plot(trials, b_err_imp, color=color3,label='VERY NOISY',ls='-',linewidth=wdt)
+    l4 = ax1.plot(trials, b_err_sham, color=color4,label='SHAM',ls='-',linewidth=wdt)
 
-    ls = l3 + l1 + l4 + l2
+    ls = l1 + l2 + l3 + l4
     labs = [l.get_label() for l in ls]
     ax1.legend(ls,labs,loc = 0)
 
     
     
+    fig_main = plt.figure()
 
+    figs=  fig_main.subfigures(1,2)
+    
+    fig = figs[0]
     # -----------------------------------------------------------------
-    color3 = np.array([1,224.0/255,179.0/255,0.5])
-    color3l = 'orange'
-    color4 = np.array([1,102.0/255.0,102.0/255.0,0.5])
-    color4l = 'red'
-    color5 = np.array([0.25,1.0,0.5,0.5])
-    color5l = 'green'
+    color1l = np.array([1,102.0/255.0,102.0/255.0,0.5])
+    color2l = np.array([102.0/255.0,102.0/255.0,1,0.5])
+    color3l = np.array([0.25,1.0,0.5,0.5])
+    color4l = np.array([1,224.0/255,179.0/255,0.5])
+    ax2 = fig.add_subplot(211,sharex=ax1)
 
-    ax3 = fig.add_subplot(212,sharex=ax1)
+    ax2.spines['right'].set_color('none')
+    ax2.spines['top'].set_color('none')
+    ax2.xaxis.set_ticks_position('bottom')
+    ax2.spines['bottom'].set_position(('data',0))
+    ax2.yaxis.set_ticks_position('left')
+    ax2.spines['left'].set_position(('data',0))
+    ax2.set_ylabel('STATE ERROR / OPTIMAL', color="Black",fontsize=10)
+
+    ax2.set_xticks([0,25,50,75,100,125,150])
+    ax2.set_yticks([1,0.75,0.5,0.25,0])
+    ax2.grid(color=gridcolor, linestyle='-',alpha = 0.2)
+
+    l1 = ax2.plot(trials,error_states,'+',color=color1l)
+    l2 = ax2.plot(trials,error_states_ok,'+',color=color2l)
+    l3 = ax2.plot(trials,error_states_imp,'+',color=color3l)
+    l4 = ax2.plot(trials,error_states_sham,'+',color=color4l)
+
+    l5 = ax2.plot(trials,state_error_mean,'-',color=color1)
+    l6 = ax2.plot(trials,state_error_mean_ok,'-',color=color2)
+    l7 = ax2.plot(trials,state_error_mean_imp,'-',color=color3)
+    l8 = ax2.plot(trials,state_error_mean_sham,'-',color=color4)
+
+    ax2.set_ylim([-0.1,1.1])
+
+
+    ax3 = fig.add_subplot(212,sharex=ax2)
     # ax3.axvline(0,color="black")
     # ax3.axhline(0,color="black")
     #ax3.grid()
@@ -228,34 +268,63 @@ if __name__=="__main__":
     ax3.yaxis.set_ticks_position('left')
     ax3.spines['left'].set_position(('data',0))
 
-    ax3.set_xticks([0,25,50,75,100,125,150])
-    ax3.set_yticks([1,0.75,0.5,0.25,0])
-    ax3.grid(color=gridcolor, linestyle='-',alpha = 0.2)
+    l10 = ax3.plot(trials,error_observations,'x',color=color1l)
+    l20 = ax3.plot(trials,error_observations_ok,'x',color=color2l)
+    l30 = ax3.plot(trials,error_observations_imp,'x',color=color3l)
+    l40 = ax3.plot(trials,error_observations_sham,'x',color=color4l)
 
-    l1 = ax3.plot(trials,error_states,'+',color=color3)
-    l2 = ax3.plot(trials,error_behaviour,'+',color=color4)
-    l3 = ax3.plot(trials,error_observations,"+",color=color5)
-    
-    l4 = ax3.plot(trials,state_error_mean,"-",color=color3l,label = 'Subject mental state errror (smoothed)',linewidth=wdt)
-    
-    l6 = ax3.plot(trials,behaviour_error_mean,"-",color=color4l,label = 'Subject behaviour error (smoothed)',linewidth=wdt)
-
-    l5 = ax3.plot(trials,error_obs_mean,"--",color=color5l,label = 'Feedback error (smoothed)',linewidth=wdt)
+    l50 = ax3.plot(trials,error_obs_mean,'--',color=color1)
+    l60 = ax3.plot(trials,error_obs_mean_ok,'--',color=color2)
+    l70 = ax3.plot(trials,error_obs_mean_imp,'--',color=color3)
+    l80 = ax3.plot(trials,error_obs_mean_sham,'--',color=color4)
 
     ls = l4 + l5 +l6
     labs = [l.get_label() for l in ls]
     ax3.legend(ls,labs,loc = 'best')
 
     ax3.set_xlabel('Trials',fontsize=15)
-    ax3.set_ylabel('PERFORMANCE ERROR / OPTIMAL', color="Black",fontsize=10)
+    ax3.set_ylabel('FEEDBACK ERROR / OPTIMAL', color="Black",fontsize=10)
     ax3.tick_params(axis='y', labelcolor='black',size= 15)
-    ax3.set_ylim([-0.1,1.1])
-    ax3.set_ylim(ax1.get_ylim()[::-1])
+    ax3.set_ylim([0,1])
+    #ax3.set_ylim(ax1.get_ylim()[::-1])
 
     fig_main.tight_layout()  # otherwise the right y-label is slightly clipped
     fig.suptitle("Trial sumup", fontsize=16,y=1.08)
 
     save_folder = os.path.join(save_path,model_name,"_RESULTS_" + "ok")
+    
+    
+    fig_2 = figs[1]
+    colors = [color1,color2,color3,color4]
+    N = 100
+    ax = fig_2.add_subplot(211)
+    ax.grid(zorder=0)
+    sop_std = np.average(np.sqrt(full_dico["variance"]["state_error"][N:,:]))
+    soo_std = np.average(np.sqrt(full_dico_ok["variance"]["state_error"][N:,:]))
+    soi_std = np.average(np.sqrt(full_dico_imp["variance"]["state_error"][N:,:]))
+    sos_std = np.average(np.sqrt(full_dico_sham["variance"]["state_error"][N:,:]))
+    so_err = [sop_std,soo_std,soi_std,sos_std]
+    ax.bar(["Perfect",'Noisy',"Very noisy","SHAM"],[np.average(error_states[N:,:]),np.average(error_states_ok[N:,:]),np.average(error_states_imp[N:,:]),np.average(error_states_sham[N:,:])],
+                yerr=so_err,color=colors,
+                zorder=3)
+    ax.set_ylim([0,1])
+    ax.set_xlabel("Feedback quality")
+    ax.set_ylabel("Hidden state error (over the 50 last trials)")
+    
+    ax = fig_2.add_subplot(212)
+    ax.grid(zorder=0)
+    eop_std = np.average(np.sqrt(full_dico["variance"]["observation_error"][N:,:]))
+    eoo_std = np.average(np.sqrt(full_dico_ok["variance"]["observation_error"][N:,:]))
+    eoi_std = np.average(np.sqrt(full_dico_imp["variance"]["observation_error"][N:,:]))
+    eos_std = np.average(np.sqrt(full_dico_sham["variance"]["observation_error"][N:,:]))
+    eo_err = [eop_std,eoo_std,eoi_std,eos_std]
+    ax.bar(["Perfect",'Noisy',"Very noisy","SHAM"],[np.average(error_observations[N:,:]),np.average(error_observations_ok[N:,:]),np.average(error_observations_imp[N:,:]),np.average(error_observations_sham[N:,:])],
+                yerr=eo_err,color=colors,
+                zorder=3)
+    ax.set_ylim([0,1])
+    ax.set_xlabel("Feedback quality")
+    ax.set_ylabel("Feedback error (over the 50 last trials)")
+    
     
     if not os.path.exists(save_folder):
                 try:
